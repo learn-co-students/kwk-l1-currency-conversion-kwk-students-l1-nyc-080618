8bb16d6b7c4270d2b371863254def49ac853e8a2
bonnabel27 <sika.attikesse@gmail.com>
2018-08-10 15:05:49 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,12 @@
-# Write your cod here
+def usd_to_eur(input)
+  coins = input.to_i * 0.88
+  return coins
+end
+
+
+def eur_to_usd (input)
+  coins = input.to_i / 0.88
+  return coins
+end
+puts eur_to_usd(200)
+
